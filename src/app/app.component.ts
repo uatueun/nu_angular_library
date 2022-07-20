70e9d6abcd96e84f0fbd0b9839ec480701df2dba
nu Pan <nu.pan@tpisoftware.com>
2022-07-20 10:33:21 +0800

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'nu_angular_library';
+}
